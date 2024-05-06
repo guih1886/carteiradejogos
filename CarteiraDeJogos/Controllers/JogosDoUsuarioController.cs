e1b0ca8f9f9d71187b2e63db2b1afc840f4be983
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-05 23:33:07 -0300

    Update JogosDoUsuarioController.cs

--- CarteiraDeJogos/Controllers/JogosDoUsuarioController.cs
+++ CarteiraDeJogos/Controllers/JogosDoUsuarioController.cs
@@ -19,7 +19,7 @@ public class JogosDoUsuarioController : ControllerBase
     public IActionResult ListarTodosOsJogos(int id)
     {
         Usuario? usuario = Utils.BuscarUsuario(id, _context);
-        if (usuario == null) return NotFound();
+        if (usuario == null) return NotFound("Usuario não encontrado.");
         if (usuario.Jogos == null) return NoContent();
         List<int> todosOsJogos = usuario.Jogos.ToList();
         return Ok(todosOsJogos);
@@ -29,9 +29,53 @@ public class JogosDoUsuarioController : ControllerBase
     public IActionResult ListarJogosFavoritos(int id)
     {
         Usuario? usuario = Utils.BuscarUsuario(id, _context);
-        if (usuario == null) return NotFound();
+        if (usuario == null) return NotFound("Usuario não encontrado.");
         if (usuario.JogosFavoritos == null) return NoContent();
         List<int> jogosFavoritos = usuario.JogosFavoritos.ToList();
         return Ok(jogosFavoritos);
+    }
+
+    [HttpPost("adicionarJogo/{idJogo}")]
+    public IActionResult AdicionarJogoUsuario(int id, int idJogo)
+    {
+        Usuario? usuario = Utils.BuscarUsuario(id, _context);
+        if (usuario == null) return NotFound("Usuario não encontrado.");
+        if (usuario.Jogos!.Contains(idJogo)) return BadRequest("Jogo já está na lista.");
+        usuario.Jogos!.Add(idJogo);
+        _context.SaveChanges();
+        return Ok(usuario.Jogos);
+    }
+
+    [HttpPost("adicionarJogoFavorito/{idJogoFavorito}")]
+    public IActionResult AdicionarJogoFavoritoUsuario(int id, int idJogoFavorito)
+    {
+        Usuario? usuario = Utils.BuscarUsuario(id, _context);
+        if (usuario == null) return NotFound("Usuario não encontrado.");
+        if (usuario.JogosFavoritos!.Contains(idJogoFavorito)) return BadRequest("Jogo já está na lista.");
+        usuario.JogosFavoritos!.Add(idJogoFavorito);
+        _context.SaveChanges();
+        return Ok(usuario.JogosFavoritos);
+    }
+
+    [HttpDelete("removerJogo/{idJogo}")]
+    public IActionResult RemoverJogoUsuario(int id, int idJogo)
+    {
+        Usuario? usuario = Utils.BuscarUsuario(id, _context);
+        if (usuario == null) return NotFound("Usuario não encontrado.");
+        if (!usuario.Jogos!.Contains(idJogo)) return BadRequest("Jogo não está na lista.");      
+        usuario.Jogos!.Remove(idJogo);
+        _context.SaveChanges();
+        return Ok(usuario.Jogos);
+    }
+
+    [HttpDelete("removerJogoFavorito/{idJogoFavorito}")]
+    public IActionResult RemoverJogoFavoritoUsuario(int id, int idJogoFavorito)
+    {
+        Usuario? usuario = Utils.BuscarUsuario(id, _context);
+        if (usuario == null) return NotFound("Usuario não encontrado.");
+        if (!usuario.JogosFavoritos!.Contains(idJogoFavorito)) return BadRequest("Jogo não está na lista.");
+        usuario.JogosFavoritos!.Remove(idJogoFavorito);
+        _context.SaveChanges();
+        return Ok(usuario.JogosFavoritos);
     }
 }
