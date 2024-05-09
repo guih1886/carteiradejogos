b6367eef3df1b851b9ee215b4f9a2c90bf7030f0
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-09 16:25:29 -0300

    Update Program.cs

--- CarteiraDeJogos/Program.cs
+++ CarteiraDeJogos/Program.cs
@@ -1,5 +1,7 @@
 using CarteiraDeJogos.Controllers;
 using CarteiraDeJogos.Data;
+using CarteiraDeJogos.Data.Interfaces;
+using CarteiraDeJogos.Data.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,15 +14,19 @@ var connectionString = builder.Configuration.GetConnectionString("CarteiraDeJogo
 builder.Services.AddDbContext<JogosContext>(opts =>
     opts.UseSqlServer(connectionString, e => e.EnableRetryOnFailure()));
 
+builder.Services.AddScoped<UsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<JogosRepository, JogosRepository>();
+builder.Services.AddScoped<JogosDoUsuarioRepository, JogosDoUsuarioRepository>();
+
 /*builder.Services.AddDbContext<JogosContext>(opts =>
     opts.UseSqlServer(connectionString, ServerVersion.AutoDetect(connectionString)));*/
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<JogosDoUsuarioController>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
