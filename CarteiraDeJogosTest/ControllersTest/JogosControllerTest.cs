bcdcda5db9a0c80376dd20fcdaf31c4f51db34ea
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-09 14:28:09 -0300

    Create JogosControllerTest.cs

---
+++ CarteiraDeJogosTest/ControllersTest/JogosControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using CarteiraDeJogos.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarteiraDeJogosTest.ControllersTest;
+
+public class JogosControllerTest
+{
+    private readonly JogosController _controller;
+
+    public JogosControllerTest(JogosController controller)
+    {
+        _controller = controller;
+    }
+
+    [Fact]
+    public void ListarJogosTest()
+    {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
