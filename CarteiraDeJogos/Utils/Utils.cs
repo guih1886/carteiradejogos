5f124f1767317eb4199a79a5ca6f70b624177846
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-09 14:28:22 -0300

    Update Utils.cs

--- CarteiraDeJogos/Utils/Utils.cs
+++ CarteiraDeJogos/Utils/Utils.cs
@@ -3,27 +3,15 @@ using CarteiraDeJogos.Models;
 
 public static class Utils
 {
-    public static Usuario? BuscarUsuario(int id, JogosContext _context)
-    {
-        Usuario? usuarioId = _context.Usuarios.FirstOrDefault(u => u.Id == id);
-        return usuarioId;
-    }
-
     public static List<Usuario>? ListarUsuarios(JogosContext _context)
     {
         List<Usuario>? usuarioId = _context.Usuarios.ToList();
         return usuarioId;
     }
 
-    public static Jogos? BuscarJogos(int id, JogosContext _context)
+    public static void AdicionarJogoUsuario(int idUsuario, int idJogo, JogosContext _context)
     {
-        Jogos? jogo = _context.Jogos.FirstOrDefault(j => j.Id == id && j.Ativo == 1);
-        return jogo;
-    }
-
-    public static void AdicionarJogoUsuario(int id, int idJogo, JogosContext _context)
-    {
-        Usuario? usuario = BuscarUsuario(id, _context);
+        Usuario? usuario =  _context.Usuarios.FirstOrDefault(u => u.Id == idUsuario);
         usuario!.Jogos!.Add(idJogo);
         _context.SaveChanges();
     }
