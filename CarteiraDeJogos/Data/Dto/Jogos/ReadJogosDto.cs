e557a274e7b25356479430ccd8f836bdb1038b1d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-10 14:48:06 -0300

    Adicionado mapeamento do json
    Adicionado mapeamento do json

--- CarteiraDeJogos/Data/Dto/Jogos/ReadJogosDto.cs
+++ CarteiraDeJogos/Data/Dto/Jogos/ReadJogosDto.cs
@@ -1,12 +1,14 @@
 ﻿using CarteiraDeJogos.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CarteiraDeJogos.Data.Dto.Jogos;
 
 public class ReadJogosDto
 {
-    public ReadJogosDto(string nome, string enderecoImagem, string descricao, string genero, string anoLancamento, string plataforma, int nota)
+    public ReadJogosDto(int id, string nome, string enderecoImagem, string descricao, string genero, string anoLancamento, string plataforma, int nota)
     {
+        Id = id;
         Nome = nome;
         EnderecoImagem = enderecoImagem;
         Descricao = descricao;
@@ -15,18 +17,20 @@ public class ReadJogosDto
         Plataforma = plataforma;
         Nota = nota;
     }
-
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("nome")]
     public string Nome { get; set; }
-
+    [JsonPropertyName("enderecoImagem")]
     public string EnderecoImagem { get; set; }
-
+    [JsonPropertyName("descricao")]
     public string Descricao { get; set; }
-
+    [JsonPropertyName("genero")]
     public string Genero { get; set; }
-
+    [JsonPropertyName("anoLancamento")]
     public string AnoLancamento { get; set; }
-
+    [JsonPropertyName("plataforma")]
     public string Plataforma { get; set; }
-
+    [JsonPropertyName("nota")]
     public int Nota { get; set; }
 }
--- CarteiraDeJogos/Data/Dto/Jogos/UpdateJogosDto.cs
+++ CarteiraDeJogos/Data/Dto/Jogos/UpdateJogosDto.cs
@@ -1,5 +1,6 @@
 ﻿using CarteiraDeJogos.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CarteiraDeJogos.Data.Dto.Jogos;
 
@@ -17,22 +18,26 @@ public class UpdateJogosDto
     }
 
     [Required(ErrorMessage = "A imagem não pode ser vazio.")]
+    [JsonPropertyName("enderecoImagem")]
     public string EnderecoImagem { get; set; }
 
     [Required(ErrorMessage = "O nome não pode ser vazio.")]
     [MinLength(3, ErrorMessage = "Nome muito curto.")]
+    [JsonPropertyName("nome")]
     public string Nome { get; set; }
 
     [Required(ErrorMessage = "A descrição não pode ser vazia.")]
     [MinLength(20, ErrorMessage = "Descrição muito curta, insira mais informações.")]
+    [JsonPropertyName("descricao")]
     public string Descricao { get; set; }
 
     [Required(ErrorMessage = "O Genero não pode ser vazio.")]
+    [JsonPropertyName("genero")]
     public Genero Genero { get; set; }
-
+    [JsonPropertyName("anoLancamento")]
     public string AnoLancamento { get; set; }
-
+    [JsonPropertyName("plataforma")]
     public string Plataforma { get; set; }
-
+    [JsonPropertyName("nota")]
     public int Nota { get; set; }
 }
