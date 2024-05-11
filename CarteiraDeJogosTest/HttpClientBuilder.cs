886e4123147d359299a16e2c3b00a9b884902815
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-11 00:46:25 -0300

    Create HttpClientBuilder.cs

---
+++ CarteiraDeJogosTest/HttpClientBuilder.cs
@@ -0,0 +1,33 @@
+﻿using CarteiraDeJogos.Data.Dto.Usuarios;
+using System.Text;
+using System.Text.Json;
+
+namespace CarteiraDeJogosTest;
+
+public class HttpClientBuilder
+{
+    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly string baseUrl = "http://localhost:5020";
+
+    public async Task<string> CadastrarAsync<T>(string endPoint, T novoObjeto)
+    {
+        string json = JsonSerializer.Serialize(novoObjeto);
+        HttpContent httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await _httpClient.PostAsync(baseUrl + endPoint, httpContent);
+        string resultado = response.Content.ReadAsStringAsync().Result;
+        return resultado;
+    }
+
+    public async Task<HttpResponseMessage> BuscarAsync(string endPoint)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(baseUrl + endPoint);
+        return response;
+    }
+
+    public async Task<HttpResponseMessage> DeletarAsync(string endPoint)
+    {
+        HttpResponseMessage response = await _httpClient.DeleteAsync(baseUrl + endPoint);
+        return response;
+    }
+}
