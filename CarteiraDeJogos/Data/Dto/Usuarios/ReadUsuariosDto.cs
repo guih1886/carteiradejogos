971acc2b1934ba98b942cb658859c97050606428
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-11 00:47:26 -0300

    Mapeamento do readusuariodto
    Mapeamento do readusuariodto

--- CarteiraDeJogos/Data/Dto/Usuarios/ReadUsuariosDto.cs
+++ CarteiraDeJogos/Data/Dto/Usuarios/ReadUsuariosDto.cs
@@ -1,17 +1,23 @@
-﻿namespace CarteiraDeJogos.Data.Dto.Usuarios;
+﻿using System.Text.Json.Serialization;
+
+namespace CarteiraDeJogos.Data.Dto.Usuarios;
 
 public class ReadUsuariosDto
 {
-    public ReadUsuariosDto(string nome, List<int> jogos, List<int> jogosFavoritos)
+    public ReadUsuariosDto(int id, string nome, List<int> jogos, List<int> jogosFavoritos)
     {
+        Id = id;
         Nome = nome;
         Jogos = jogos;
         JogosFavoritos = jogosFavoritos;
     }
 
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("nome")]
     public string Nome { get; set; }
-
+    [JsonPropertyName("jogos")]
     public List<int> Jogos { get; set; }
-
+    [JsonPropertyName("jogosFavoritos")]
     public List<int> JogosFavoritos { get; set; }
 }
