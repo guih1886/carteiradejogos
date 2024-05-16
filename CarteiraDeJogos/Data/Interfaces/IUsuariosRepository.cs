ca8129485aef58112097eefb48a3f2779c70a3f1
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-16 13:47:04 -0300

    Update IUsuariosRepository.cs

--- CarteiraDeJogos/Data/Interfaces/IUsuariosRepository.cs
+++ CarteiraDeJogos/Data/Interfaces/IUsuariosRepository.cs
@@ -1,12 +1,19 @@
 ﻿using CarteiraDeJogos.Data.Dto.Usuarios;
+using CarteiraDeJogos.Models;
 
 namespace CarteiraDeJogos.Data.Interfaces;
 
 public interface IUsuariosRepository
 {
-    List<ReadUsuariosDto> ListarUsuarios();
+    Usuario? BuscarUsuario(int id);
     ReadUsuariosDto BuscarUsuarioPorId(int id);
+    List<ReadUsuariosDto> ListarUsuarios();
     ReadUsuariosDto CadastrarUsuario(CreateUsuarioDto usuarioDto);
     ReadUsuariosDto AtualizarUsuario(int id, UpdateUsuariosDto usuarioDto);
     bool DeletarUsuario(int id);
+    string AdicionarJogoUsuario(int usuarioId, int id);
+    string AdicionarJogoFavorito(int usuarioId, int idJogoFavorito);
+    string RemoverJogo(int usuarioId, int idJogo);
+    string RemoverJogoFavorito(int usuarioId, int idJogo);
+    Usuario? BuscarUsuarioEmail(string email);
 }
