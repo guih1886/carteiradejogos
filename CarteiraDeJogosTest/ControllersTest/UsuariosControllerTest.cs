169a77ca1f1948bfd59af8113bf8ef2f6a51538d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-18 13:42:11 -0300

    Finalização dos testes de usuários
    Finalização dos testes de usuários

--- CarteiraDeJogosTest/ControllersTest/UsuariosControllerTest.cs
+++ CarteiraDeJogosTest/ControllersTest/UsuariosControllerTest.cs
@@ -1,139 +1,147 @@
-﻿using CarteiraDeJogos.Data.Dto.Usuarios;
-using System.Net;
-using System.Text.Json;
+﻿using CarteiraDeJogos.Controllers;
+using CarteiraDeJogos.Data.Dto.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Xunit.Abstractions;
 
 namespace CarteiraDeJogosTest.ControllersTest
 {
     public class UsuariosControllerTest
     {
-        private HttpClientBuilder _httpClientBuilder = new HttpClientBuilder();
+        private UsuarioController controller;
         private ITestOutputHelper _outputHelper;
 
         public UsuariosControllerTest(ITestOutputHelper outputHelper)
         {
+            controller = new UsuarioServiceProvider().AdicionarServico<UsuarioController>()!;
             _outputHelper = outputHelper;
         }
 
         [Fact]
-        public async void CadastrarUsuarioTest()
+        public void CadastrarUsuarioTest()
         {
             //Arrange
             CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José", "guilherme@gmail.com", "1234", "1234");
             //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.CadastrarAsync<CreateUsuarioDto>("/Usuarios", novoUsuario);
-            ReadUsuariosDto usuario = JsonSerializer.Deserialize<ReadUsuariosDto>(resposta.Content.ReadAsStringAsync().Result)!;
-            _outputHelper.WriteLine(usuario.Id.ToString());
+            var resposta = controller.CadastrarUsuario(novoUsuario);
+            ReadUsuariosDto usuario = JsonConvert.DeserializeObject<ReadUsuariosDto>(resposta.Value.ToString());
             //Assert
-            Assert.Equal("Malaquias José", usuario.Nome);
-            await _httpClientBuilder.DeletarAsync($"/Usuarios/{usuario.Id}");
+            _outputHelper.WriteLine(usuario.ToString());
+            Assert.Equal(200, resposta.StatusCode);
+            Assert.IsType<ReadUsuariosDto>(usuario);
+            DeletarUsuario(usuario);
         }
-
-        [Fact]
-        public async void CadastrarUsuarioIncorretoTest()
+        [Fact(Skip = "Atualizar validação para o modelo")]
+        public void CadastrarUsuarioIncorretoTest()
         {
             //Arrange
-            CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José", "guilherme@gmail.com", "123456", "1234");
+            CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José", "guilherme@gmail.com", "", "1234");
             //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.CadastrarAsync<CreateUsuarioDto>("/Usuarios", novoUsuario);
-            _outputHelper.WriteLine(resposta.Content.ReadAsStringAsync().Result);
+            var resposta = controller.CadastrarUsuario(novoUsuario);
+            ReadUsuariosDto usuario = JsonConvert.DeserializeObject<ReadUsuariosDto>(resposta.Value.ToString());
+            _outputHelper.WriteLine(usuario.ToString());
             //Assert
-            Assert.Contains("As senhas não são iguais.", resposta.Content.ReadAsStringAsync().Result);
+            Assert.Equal(400, resposta.StatusCode);
+            Assert.Equal("Erro ao cadastrar usuário.", resposta.Value);
+            DeletarUsuario(usuario);
         }
-
         [Fact]
-        public async void AlterarUsuarioTest()
+        public void AlterarUsuarioTest()
         {
             //Arrange
-            CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José", "guilherme@gmail.com", "1234", "1234");
+            ReadUsuariosDto usuario = CriarUsuario();
             UpdateUsuariosDto usuarioAlterado = new UpdateUsuariosDto("Usuario Alterado", [1, 2, 5, 8], [1, 2]);
             //Act
-            HttpResponseMessage novoUsuarioDTO = await _httpClientBuilder.CadastrarAsync<CreateUsuarioDto>("/Usuarios", novoUsuario);
-            ReadUsuariosDto readNovoUsuarioDTO = JsonSerializer.Deserialize<ReadUsuariosDto>(novoUsuarioDTO.Content.ReadAsStringAsync().Result)!;
-
-            HttpResponseMessage resposta = await _httpClientBuilder.AlterarAsync<UpdateUsuariosDto>($"/Usuarios/{readNovoUsuarioDTO.Id}", usuarioAlterado);
-            ReadUsuariosDto usuario = JsonSerializer.Deserialize<ReadUsuariosDto>(resposta.Content.ReadAsStringAsync().Result)!;
-            _outputHelper.WriteLine(usuario.Nome);
+            ObjectResult resposta = controller.AlterarUsuario(usuario.Id, usuarioAlterado);
+            ReadUsuariosDto alterado = JsonConvert.DeserializeObject<ReadUsuariosDto>(resposta.Value.ToString());
+            _outputHelper.WriteLine(alterado.ToString());
             //Assert
-            Assert.Equal("Usuario Alterado", usuario.Nome);
-            await _httpClientBuilder.DeletarAsync($"/Usuarios/{readNovoUsuarioDTO.Id}");
+            Assert.Equal(200, resposta.StatusCode);
+            Assert.Equal("Usuario Alterado", alterado.Nome);
+            DeletarUsuario(usuario);
         }
-
         [Fact]
-        public async void AlterarUsuarioIncorretoTest()
+        public void AlterarUsuarioIncorretoTest()
         {
             //Arrange
             UpdateUsuariosDto novoUsuario = new UpdateUsuariosDto("Usuario Alterado", [1, 2, 5, 8], [1, 2]);
             //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.AlterarAsync<UpdateUsuariosDto>("/Usuarios/0", novoUsuario);
-            _outputHelper.WriteLine(resposta.Content.ReadAsStringAsync().Result);
+            ObjectResult resposta = controller.AlterarUsuario(0, novoUsuario);
+            _outputHelper.WriteLine(resposta.Value.ToString());
             //Assert
-            Assert.Contains("Not Found", resposta.Content.ReadAsStringAsync().Result);
+            Assert.Equal(404, resposta.StatusCode);
+            Assert.Equal("Usuário não encontrado.", resposta.Value);
         }
-
         [Fact]
-        public async void ListarUsuariosTest()
+        public void ListarUsuariosTest()
         {
             //Arrange
             //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.BuscarAsync("/Usuarios");
-            _outputHelper.WriteLine(resposta.ToString());
+            ObjectResult resposta = controller.ListarUsuarios();
+            _outputHelper.WriteLine(resposta.Value.ToString());
             //Assert
-            Assert.Equal(HttpStatusCode.OK, resposta.StatusCode);
+            Assert.Equal(200, resposta.StatusCode);
+            Assert.IsType<List<ReadUsuariosDto>>(resposta.Value);
         }
-
         [Fact]
-        public async void ListarUsuarioPorIdTest()
+        public void ListarUsuarioPorIdIncorretoTest()
         {
             //Arrange
+            //Act
+            ObjectResult resposta = controller.BuscarUsuarioPorId(0);
+            _outputHelper.WriteLine(resposta.Value.ToString());
+            //Assert
+            Assert.Equal(404, resposta.StatusCode);
+            Assert.Equal("Usuário não encontrado.", resposta.Value);
+        }
+        [Fact]
+        public void ListarUsuarioPorIdTest()
+        {
+            //Arrange
+            ReadUsuariosDto usuario = CriarUsuario();
+            //Act
+            var resposta = controller.BuscarUsuarioPorId(usuario.Id);
+            _outputHelper.WriteLine(resposta.Value.ToString());
+            //Assert
+            Assert.Equal(200, resposta.StatusCode);
+            DeletarUsuario(usuario);
+        }
+        [Fact]
+        public void DeletarUsuarioTest()
+        {
+            //Arrange
+            ReadUsuariosDto usuario = CriarUsuario();
+            //Act
+            ObjectResult resposta = controller.DeletarUsuario(usuario.Id);
+            //Assert
+            Assert.Equal(204, resposta.StatusCode);
+            Assert.Equal("Usuário excluido com sucesso.", resposta.Value);
+            DeletarUsuario(usuario);
+        }
+        [Fact]
+        public void DeletarUsuarioIncorretoTest()
+        {
+            //Arrange
+            //Act
+            ObjectResult resposta = controller.DeletarUsuario(0);
+            _outputHelper.WriteLine(resposta.Value.ToString());
+            //Assert
+            Assert.Equal(404, resposta.StatusCode);
+            Assert.Equal("Usuário não encontrado.", resposta.Value);
+        }
+
+        private ReadUsuariosDto CriarUsuario()
+        {
             CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José", "guilherme@gmail.com", "1234", "1234");
-            //Act
-            //Act
-            HttpResponseMessage usuarioCriado = await _httpClientBuilder.CadastrarAsync<CreateUsuarioDto>("/Usuarios", novoUsuario);
-            ReadUsuariosDto usuarioCriadoReadDto = JsonSerializer.Deserialize<ReadUsuariosDto>(usuarioCriado.Content.ReadAsStringAsync().Result)!;
-            HttpResponseMessage resposta = await _httpClientBuilder.BuscarAsync($"/Usuarios/{usuarioCriadoReadDto.Id}");
-            ReadUsuariosDto usuario = JsonSerializer.Deserialize<ReadUsuariosDto>(resposta.Content.ReadAsStringAsync().Result)!;
-            _outputHelper.WriteLine(usuario.Id.ToString());
-            //Assert
-            Assert.Equal(HttpStatusCode.OK, resposta.StatusCode);
-            Assert.Equal(usuarioCriadoReadDto.Id, usuario.Id);
-            await _httpClientBuilder.DeletarAsync($"/Usuarios/{usuario.Id}");
+            ObjectResult resposta = controller.CadastrarUsuario(novoUsuario);
+            ReadUsuariosDto usuario = JsonConvert.DeserializeObject<ReadUsuariosDto>(resposta.Value.ToString()!)!;
+            _outputHelper.WriteLine($"Usuario {usuario.Id} criado com sucesso.");
+            return usuario;
         }
-
-        [Fact]
-        public async void ListarUsuarioPorIdIncorretoTest()
+        private void DeletarUsuario(ReadUsuariosDto usuario)
         {
-            //Arrange
-            //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.BuscarAsync($"/Usuarios/0");
-            _outputHelper.WriteLine(resposta.ToString());
-            //Assert
-            Assert.Equal(HttpStatusCode.NotFound, resposta.StatusCode);
-        }
-
-        [Fact]
-        public async void DeletarUsuarioTest()
-        {
-            //Arrange
-            CreateUsuarioDto novoUsuario = new CreateUsuarioDto("Malaquias José","guilherme@gmail.com", "1234", "1234");
-            //Act
-            HttpResponseMessage usuarioCriado = await _httpClientBuilder.CadastrarAsync<CreateUsuarioDto>("/Usuarios", novoUsuario);
-            ReadUsuariosDto usuario = JsonSerializer.Deserialize<ReadUsuariosDto>(usuarioCriado.Content.ReadAsStringAsync().Result)!;
-            HttpResponseMessage resposta = await _httpClientBuilder.DeletarAsync($"/Usuarios/{usuario.Id}");
-            _outputHelper.WriteLine(resposta.ToString());
-            //Assert
-            Assert.Equal(HttpStatusCode.NoContent, resposta.StatusCode);
-        }
-
-        [Fact]
-        public async void DeletarUsuarioIncorretoTest()
-        {
-            //Arrange
-            //Act
-            HttpResponseMessage resposta = await _httpClientBuilder.DeletarAsync($"/Usuarios/0");
-            _outputHelper.WriteLine(resposta.ToString());
-            //Assert
-            Assert.Equal(HttpStatusCode.NotFound, resposta.StatusCode);
+            controller.DeletarUsuario(usuario.Id);
+            _outputHelper.WriteLine($"Usuario deletado {usuario.Id} com sucesso.");
         }
     }
 }
