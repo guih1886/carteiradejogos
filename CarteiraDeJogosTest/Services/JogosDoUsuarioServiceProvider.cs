668b875923e5ccb0f2e51d95b3196b415adc2fa1
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-20 22:34:08 -0300

    Create JogosDoUsuarioServiceProvider.cs

---
+++ CarteiraDeJogosTest/Services/JogosDoUsuarioServiceProvider.cs
@@ -0,0 +1,38 @@
+﻿using CarteiraDeJogos.Controllers;
+using CarteiraDeJogos.Data.Interfaces;
+using CarteiraDeJogos.Data.Repository;
+using CarteiraDeJogos.Data;
+using CarteiraDeJogos.Profiles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarteiraDeJogosTest.Services
+{
+    public class JogosDoUsuarioServiceProvider
+    {
+        public JogosDoUsuarioController AdicionarServico()
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = configuration.GetConnectionString("CarteiraDeJogosTestConnection");
+
+            var servico = new ServiceCollection();
+            servico.AddSingleton<IConfiguration>(configuration);
+            servico.AddDbContext<JogosContext>(opts =>
+                opts.UseSqlServer(connectionString, e => e.EnableRetryOnFailure()));
+
+            servico.AddTransient<IUsuariosRepository, UsuarioRepository>();
+            servico.AddTransient<JogosDoUsuarioController>();
+            servico.AddTransient<IJogosRepository, JogosRepository>();
+            servico.AddAutoMapper(typeof(UsuariosProfile).Assembly);
+            servico.AddAutoMapper(typeof(JogosProfile).Assembly);
+            var provedor = servico.BuildServiceProvider();
+            var controler = provedor.GetService<JogosDoUsuarioController>();
+            return controler;
+        }
+    }
+}
