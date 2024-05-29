2c009e2b1d50f20dd946654d8f24b4cdbb584604
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-29 17:54:53 -0300

    Criação da classe de validação de email
    Criação da classe de validação de email

---
+++ CarteiraDeJogosForms/Classes/Utils/ValidaEmail.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CarteiraDeJogosForms.Classes.Utils;
+
+public static class ValidaEmail
+{
+    public static bool ValidarEmail(string email)
+    {
+        string emailRegex = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        return Regex.IsMatch(email, emailRegex, RegexOptions.IgnoreCase);
+    }
+}
