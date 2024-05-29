79c26512a536a1512e926b861b54cfe27c0e2239
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-28 21:13:52 -0300

    Criação da classe de requisições
    Criação da classe de requisições

---
+++ CarteiraDeJogosForms/Classes/HttpClientBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CarteiraDeJogosForms.Classes
+{
+    public class HttpClientBuilder
+    {
+        private HttpClient _httpClient = new HttpClient();
+        private string urlBase = "https://localhost:5020";
+
+        public async Task<HttpResponseMessage> PostReq(string endPoint, HttpContent body)
+        {
+            HttpResponseMessage resposta = await _httpClient.PostAsync(urlBase + endPoint, body);
+            return resposta;
+        }
+    }
+}
