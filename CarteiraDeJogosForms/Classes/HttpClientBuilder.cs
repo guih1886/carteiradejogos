a891175cadbb0936d956bf952bc2c09bcf3354fc
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-29 17:54:33 -0300

    Refatoração do método post
    Refatoração do método post

--- CarteiraDeJogosForms/Classes/HttpClientBuilder.cs
+++ CarteiraDeJogosForms/Classes/HttpClientBuilder.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace CarteiraDeJogosForms.Classes
 {
@@ -7,9 +9,11 @@ namespace CarteiraDeJogosForms.Classes
         private HttpClient _httpClient = new HttpClient();
         private string urlBase = "https://localhost:5020";
 
-        public async Task<HttpResponseMessage> PostReq(string endPoint, HttpContent body)
+        public async Task<HttpResponseMessage> PostReq(string endPoint, object body)
         {
-            HttpResponseMessage resposta = await _httpClient.PostAsync(urlBase + endPoint, body);
+            string json = JsonConvert.SerializeObject(body);
+            var content = new StringContent(json, Encoding.UTF8, new MediaTypeHeaderValue("application/json"));
+            HttpResponseMessage resposta = await _httpClient.PostAsync(urlBase + endPoint, content);
             return resposta;
         }
     }
