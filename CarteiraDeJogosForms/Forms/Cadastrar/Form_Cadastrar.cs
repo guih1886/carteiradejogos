e113c0b98369666d142180f8848f80b351c0c703
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-29 18:44:56 -0300

    Refatoração do formulario de cadastro de usuarios
    Refatoração do formulario de cadastro de usuarios

--- CarteiraDeJogosForms/Forms/Cadastrar/Form_Cadastrar.cs
+++ CarteiraDeJogosForms/Forms/Cadastrar/Form_Cadastrar.cs
@@ -21,36 +21,20 @@ namespace CarteiraDeJogosForms.Forms.Cadastrar
         }
         private async void Btn_Cadastrar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(Txt_Nome.Text) || string.IsNullOrEmpty(Txt_Email.Text) || string.IsNullOrEmpty(Txt_Senha.Text) || string.IsNullOrEmpty(Txt_ConfirmaSenha.Text))
+            CreateUsuarioDto usuario = new CreateUsuarioDto(Txt_Nome.Text, Txt_Email.Text, Txt_Senha.Text, Txt_ConfirmaSenha.Text);
+            HttpResponseMessage resposta = await _httpClientBuilder.PostReq("/Usuarios", usuario);
+            string msg = await ValidaRequisicao.CadastrarUsuario(resposta);
+            if (msg.Contains("Ok"))
             {
-                Lbl_Erro.Text = "Preencha todos os campos.";
-            }
-            else if (!ValidaEmail.ValidarEmail(Txt_Email.Text))
-            {
-                Lbl_Erro.Text = "E-mail inválido.";
-            }
-            else if (Txt_Senha.Text != Txt_ConfirmaSenha.Text)
-            {
-                Lbl_Erro.Text = "As senhas não são iguais.";
+                Lbl_Erro.Text = "";
+                MessageBox.Show("Usuário cadastrado com sucesso", "Cadastro de Usuários", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                loginForm.Show();
+                this.Close();
             }
             else
             {
-                CreateUsuarioDto usuario = new CreateUsuarioDto(Txt_Nome.Text, Txt_Email.Text, Txt_Senha.Text, Txt_ConfirmaSenha.Text);
-                HttpResponseMessage resposta = await _httpClientBuilder.PostReq("/Usuarios", usuario);
-                string msg = await resposta.Content.ReadAsStringAsync();
-                if (msg.Contains(Txt_Nome.Text))
-                {
-                    Lbl_Erro.Text = "";
-                    MessageBox.Show("Usuário cadastrado com sucesso", "Cadastro de Usuários", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    loginForm.Show();
-                    this.Close();
-                }
-                else
-                {
-                    Lbl_Erro.Text = msg;
-                }
+                Lbl_Erro.Text = msg;
             }
         }
-
     }
 }
