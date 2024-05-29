31275e4b05185896aed80baa1213ab1e9589ee00
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-28 21:13:32 -0300

    Criação do formulário principal
    Criação do formulário principal

---
+++ CarteiraDeJogosForms/Forms/Form_Principal.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CarteiraDeJogosForms.Forms
+{
+    partial class Form_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            primeiroToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { primeiroToolStripMenuItem, sairToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(38, 20);
+            sairToolStripMenuItem.Text = "Sair";
+            sairToolStripMenuItem.Click += sairToolStripMenuItem_Click;
+            // 
+            // primeiroToolStripMenuItem
+            // 
+            primeiroToolStripMenuItem.Name = "primeiroToolStripMenuItem";
+            primeiroToolStripMenuItem.Size = new Size(64, 20);
+            primeiroToolStripMenuItem.Text = "primeiro";
+            // 
+            // Form_Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form_Principal";
+            Text = "Form_Principal";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem primeiroToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ CarteiraDeJogosForms/Forms/Form_Principal.cs
@@ -0,0 +1,22 @@
+﻿namespace CarteiraDeJogosForms.Forms
+{
+    public partial class Form_Principal : Form
+    {
+        private Form loginForm;
+        public Form_Principal(Form login)
+        {
+            InitializeComponent();
+            loginForm = login;
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\Windows\\Temp\\jwt.txt"))
+            {
+                File.Delete("C:\\Windows\\Temp\\jwt.txt");
+            }
+            loginForm.Show();
+            this.Close();
+        }
+    }
+}
