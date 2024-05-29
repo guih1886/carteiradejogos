cf90470c48ee8e4f4658dead1aab370875ac6b21
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-28 21:13:15 -0300

    Criação do formulário de login
    Criação do formulário de login

---
+++ CarteiraDeJogosForms/Forms/Form_Login.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CarteiraDeJogosForms.Forms
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Btn_Entrar = new Button();
+            Btn_Sair = new Button();
+            Lbl_Erro = new Label();
+            Pic_Login = new PictureBox();
+            Txt_Email = new TextBox();
+            Txt_Senha = new TextBox();
+            Lbl_Titulo = new Label();
+            ((System.ComponentModel.ISupportInitialize)Pic_Login).BeginInit();
+            SuspendLayout();
+            // 
+            // Btn_Entrar
+            // 
+            Btn_Entrar.Location = new Point(105, 217);
+            Btn_Entrar.Name = "Btn_Entrar";
+            Btn_Entrar.Size = new Size(99, 23);
+            Btn_Entrar.TabIndex = 3;
+            Btn_Entrar.Text = "Entrar";
+            Btn_Entrar.UseVisualStyleBackColor = true;
+            Btn_Entrar.Click += Btn_Entrar_Click;
+            // 
+            // Btn_Sair
+            // 
+            Btn_Sair.Location = new Point(194, 269);
+            Btn_Sair.Name = "Btn_Sair";
+            Btn_Sair.Size = new Size(100, 23);
+            Btn_Sair.TabIndex = 4;
+            Btn_Sair.Text = "Sair";
+            Btn_Sair.UseVisualStyleBackColor = true;
+            Btn_Sair.Click += Btn_Sair_Click;
+            // 
+            // Lbl_Erro
+            // 
+            Lbl_Erro.Location = new Point(12, 243);
+            Lbl_Erro.Name = "Lbl_Erro";
+            Lbl_Erro.Size = new Size(282, 23);
+            Lbl_Erro.TabIndex = 0;
+            Lbl_Erro.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Pic_Login
+            // 
+            Pic_Login.Image = Properties.Resources.login;
+            Pic_Login.Location = new Point(11, 34);
+            Pic_Login.Name = "Pic_Login";
+            Pic_Login.Size = new Size(282, 119);
+            Pic_Login.SizeMode = PictureBoxSizeMode.Zoom;
+            Pic_Login.TabIndex = 3;
+            Pic_Login.TabStop = false;
+            // 
+            // Txt_Email
+            // 
+            Txt_Email.Location = new Point(12, 159);
+            Txt_Email.Name = "Txt_Email";
+            Txt_Email.PlaceholderText = "E-mail";
+            Txt_Email.Size = new Size(282, 23);
+            Txt_Email.TabIndex = 1;
+            // 
+            // Txt_Senha
+            // 
+            Txt_Senha.Location = new Point(12, 188);
+            Txt_Senha.Name = "Txt_Senha";
+            Txt_Senha.PasswordChar = '*';
+            Txt_Senha.PlaceholderText = "Senha";
+            Txt_Senha.Size = new Size(282, 23);
+            Txt_Senha.TabIndex = 2;
+            // 
+            // Lbl_Titulo
+            // 
+            Lbl_Titulo.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Lbl_Titulo.Location = new Point(11, 1);
+            Lbl_Titulo.Name = "Lbl_Titulo";
+            Lbl_Titulo.Size = new Size(282, 30);
+            Lbl_Titulo.TabIndex = 5;
+            Lbl_Titulo.Text = "Carteira de Jogos";
+            Lbl_Titulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form_Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(305, 300);
+            Controls.Add(Lbl_Titulo);
+            Controls.Add(Txt_Senha);
+            Controls.Add(Txt_Email);
+            Controls.Add(Pic_Login);
+            Controls.Add(Lbl_Erro);
+            Controls.Add(Btn_Sair);
+            Controls.Add(Btn_Entrar);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form_Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form_Login";
+            ((System.ComponentModel.ISupportInitialize)Pic_Login).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Btn_Entrar;
+        private Button Btn_Sair;
+        private Label Lbl_Erro;
+        private PictureBox Pic_Login;
+        private TextBox Txt_Email;
+        private TextBox Txt_Senha;
+        private Label Lbl_Titulo;
+    }
+}
---
+++ CarteiraDeJogosForms/Forms/Form_Login.cs
@@ -0,0 +1,55 @@
+﻿using CarteiraDeJogos.Data.Dto.Usuarios;
+using CarteiraDeJogosForms.Classes;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CarteiraDeJogosForms.Forms
+{
+    public partial class Form_Login : Form
+    {
+        private HttpClientBuilder _httpClient = new HttpClientBuilder();
+
+        public Form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_Entrar_Click(object sender, EventArgs e)
+        {
+            LoginUsuarioDto usuario = new LoginUsuarioDto(Txt_Email.Text, Txt_Senha.Text);
+            string json = JsonConvert.SerializeObject(usuario);
+            var content = new StringContent(json, Encoding.UTF8, new MediaTypeHeaderValue("application/json"));
+            var resposta = await _httpClient.PostReq("/Login", content);
+            if (resposta.StatusCode != HttpStatusCode.OK)
+            {
+                Txt_Email.Text = "";
+                Txt_Senha.Text = "";
+                Txt_Email.Focus();
+                Lbl_Erro.ForeColor = Color.Red;
+                Lbl_Erro.Text = await resposta.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                string jwt = await resposta.Content.ReadAsStringAsync();
+                File.WriteAllText("C:\\Windows\\Temp\\jwt.txt", jwt);
+                Form_Principal form_Principal = new Form_Principal(this);
+                form_Principal.Show();
+                Lbl_Erro.Text = "";
+                Txt_Email.Text = "";
+                Txt_Senha.Text = "";
+                Txt_Email.Focus();
+                this.Hide();
+            }
+        }
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\Windows\\Temp\\jwt.txt"))
+            {
+                File.Delete("C:\\Windows\\Temp\\jwt.txt");
+            }
+            this.Close();
+        }
+    }
+}
