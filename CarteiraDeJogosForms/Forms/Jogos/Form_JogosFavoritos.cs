adbfd32c0f7599240ae65b7b36bd4dfa105f16fe
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-30 16:13:05 -0300

    Criação formulário de jogos favoritos
    Criação formulário de jogos favoritos

---
+++ CarteiraDeJogosForms/Forms/Jogos/Form_JogosFavoritos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarteiraDeJogosForms.Forms.Jogos
+{
+    public partial class Form_JogosFavoritos : Form
+    {
+        public Form_JogosFavoritos()
+        {
+            InitializeComponent();
+        }
+    }
+}
