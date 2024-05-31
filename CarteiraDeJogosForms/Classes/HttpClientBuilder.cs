9c7ed134d23aa1eab8deaa2136d35914d0e3314e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-31 20:27:01 -0300

    Atualização do httpclientbuilder
    Atualização do httpclientbuilder

--- CarteiraDeJogosForms/Classes/HttpClientBuilder.cs
+++ CarteiraDeJogosForms/Classes/HttpClientBuilder.cs
@@ -1,5 +1,4 @@
-﻿using CarteiraDeJogos.Models;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
 
@@ -7,14 +6,30 @@ namespace CarteiraDeJogosForms.Classes
 {
     public class HttpClientBuilder
     {
-        private HttpClient _httpClient = new HttpClient();
-        private string urlBase = "https://localhost:5020";
+        private HttpClient _httpClient;
+        private string urlBase;
+        private string jwt;
+
+        public HttpClientBuilder(string urlBase, string jwt)
+        {
+            _httpClient = new HttpClient();
+            this.urlBase = urlBase;
+            this.jwt = jwt;
+        }
 
         public async Task<HttpResponseMessage> PostReq(string endPoint, object body)
         {
             string json = JsonConvert.SerializeObject(body);
             var content = new StringContent(json, Encoding.UTF8, new MediaTypeHeaderValue("application/json"));
             HttpResponseMessage resposta = await _httpClient.PostAsync(urlBase + endPoint, content);
+            return resposta;
+        }
+        public async Task<HttpResponseMessage> PutReq(string endPoint, object body)
+        {
+            string json = JsonConvert.SerializeObject(body);
+            var content = new StringContent(json, Encoding.UTF8, new MediaTypeHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+            HttpResponseMessage resposta = await _httpClient.PutAsync(urlBase + endPoint, content);
             return resposta;
         }
         public async Task<HttpResponseMessage> GetJogosDoUsuario(int usuarioId)
@@ -24,7 +39,7 @@ namespace CarteiraDeJogosForms.Classes
         }
         public async Task<HttpResponseMessage> GetJogosFavoritosDoUsuario(int usuarioId)
         {
-            HttpResponseMessage resposta = await _httpClient.GetAsync(urlBase + $"/JogosDoUsuario/{usuarioId}/todosfavoritos");
+            HttpResponseMessage resposta = await _httpClient.GetAsync(urlBase + $"/JogosDoUsuario/{usuarioId}/jogosfavoritos");
             return resposta;
         }
         public async Task<HttpResponseMessage> GetJogo(int id)
@@ -32,5 +47,11 @@ namespace CarteiraDeJogosForms.Classes
             HttpResponseMessage resposta = await _httpClient.GetAsync(urlBase + $"/Jogos/{id}");
             return resposta;
         }
+        public async Task<HttpResponseMessage> GetUsuario(int usuarioId)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync(urlBase + $"/Usuarios/{usuarioId}");
+            return resposta;
+        }
+
     }
 }
