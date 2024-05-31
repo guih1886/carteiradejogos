0a0190cf75c2c539fc14a594f8e023313d8aede2
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-31 20:26:21 -0300

    Atualização do formulário de jogos favoritos
    Atualização do formulário de jogos favoritos

--- CarteiraDeJogosForms/Forms/Jogos/Form_JogosFavoritos.Designer.cs
+++ CarteiraDeJogosForms/Forms/Jogos/Form_JogosFavoritos.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_JogosFavoritos";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_JogosFavoritos));
+            Dgv_JogosFavoritos = new DataGridView();
+            idDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nomeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            descricaoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            generoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            enderecoImagemDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            anoLancamentoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            plataformaDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            notaDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            ativoDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            readJogosDtoBindingSource = new BindingSource(components);
+            Lbl_Total = new Label();
+            ((System.ComponentModel.ISupportInitialize)Dgv_JogosFavoritos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)readJogosDtoBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // Dgv_JogosFavoritos
+            // 
+            Dgv_JogosFavoritos.AllowUserToAddRows = false;
+            Dgv_JogosFavoritos.AllowUserToDeleteRows = false;
+            Dgv_JogosFavoritos.AutoGenerateColumns = false;
+            Dgv_JogosFavoritos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Dgv_JogosFavoritos.Columns.AddRange(new DataGridViewColumn[] { idDataGridViewTextBoxColumn, nomeDataGridViewTextBoxColumn, descricaoDataGridViewTextBoxColumn, generoDataGridViewTextBoxColumn, enderecoImagemDataGridViewTextBoxColumn, anoLancamentoDataGridViewTextBoxColumn, plataformaDataGridViewTextBoxColumn, notaDataGridViewTextBoxColumn, ativoDataGridViewTextBoxColumn });
+            Dgv_JogosFavoritos.DataSource = readJogosDtoBindingSource;
+            Dgv_JogosFavoritos.Location = new Point(0, 0);
+            Dgv_JogosFavoritos.Name = "Dgv_JogosFavoritos";
+            Dgv_JogosFavoritos.ReadOnly = true;
+            Dgv_JogosFavoritos.Size = new Size(1108, 535);
+            Dgv_JogosFavoritos.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            idDataGridViewTextBoxColumn.Frozen = true;
+            idDataGridViewTextBoxColumn.HeaderText = "Id";
+            idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            idDataGridViewTextBoxColumn.ReadOnly = true;
+            idDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            nomeDataGridViewTextBoxColumn.ReadOnly = true;
+            nomeDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // descricaoDataGridViewTextBoxColumn
+            // 
+            descricaoDataGridViewTextBoxColumn.DataPropertyName = "Descricao";
+            descricaoDataGridViewTextBoxColumn.HeaderText = "Descricao";
+            descricaoDataGridViewTextBoxColumn.Name = "descricaoDataGridViewTextBoxColumn";
+            descricaoDataGridViewTextBoxColumn.ReadOnly = true;
+            descricaoDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // generoDataGridViewTextBoxColumn
+            // 
+            generoDataGridViewTextBoxColumn.DataPropertyName = "Genero";
+            generoDataGridViewTextBoxColumn.HeaderText = "Genero";
+            generoDataGridViewTextBoxColumn.Name = "generoDataGridViewTextBoxColumn";
+            generoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // enderecoImagemDataGridViewTextBoxColumn
+            // 
+            enderecoImagemDataGridViewTextBoxColumn.DataPropertyName = "EnderecoImagem";
+            enderecoImagemDataGridViewTextBoxColumn.HeaderText = "Imagem";
+            enderecoImagemDataGridViewTextBoxColumn.Name = "enderecoImagemDataGridViewTextBoxColumn";
+            enderecoImagemDataGridViewTextBoxColumn.ReadOnly = true;
+            enderecoImagemDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // anoLancamentoDataGridViewTextBoxColumn
+            // 
+            anoLancamentoDataGridViewTextBoxColumn.DataPropertyName = "AnoLancamento";
+            anoLancamentoDataGridViewTextBoxColumn.HeaderText = "Lançamento";
+            anoLancamentoDataGridViewTextBoxColumn.Name = "anoLancamentoDataGridViewTextBoxColumn";
+            anoLancamentoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // plataformaDataGridViewTextBoxColumn
+            // 
+            plataformaDataGridViewTextBoxColumn.DataPropertyName = "Plataforma";
+            plataformaDataGridViewTextBoxColumn.HeaderText = "Plataforma";
+            plataformaDataGridViewTextBoxColumn.Name = "plataformaDataGridViewTextBoxColumn";
+            plataformaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // notaDataGridViewTextBoxColumn
+            // 
+            notaDataGridViewTextBoxColumn.DataPropertyName = "Nota";
+            notaDataGridViewTextBoxColumn.HeaderText = "Nota";
+            notaDataGridViewTextBoxColumn.Name = "notaDataGridViewTextBoxColumn";
+            notaDataGridViewTextBoxColumn.ReadOnly = true;
+            notaDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // ativoDataGridViewTextBoxColumn
+            // 
+            ativoDataGridViewTextBoxColumn.DataPropertyName = "Ativo";
+            ativoDataGridViewTextBoxColumn.HeaderText = "Ativo";
+            ativoDataGridViewTextBoxColumn.Name = "ativoDataGridViewTextBoxColumn";
+            ativoDataGridViewTextBoxColumn.ReadOnly = true;
+            ativoDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // readJogosDtoBindingSource
+            // 
+            readJogosDtoBindingSource.DataSource = typeof(CarteiraDeJogos.Data.Dto.Jogos.ReadJogosDto);
+            // 
+            // Lbl_Total
+            // 
+            Lbl_Total.Location = new Point(0, 538);
+            Lbl_Total.Name = "Lbl_Total";
+            Lbl_Total.Size = new Size(366, 23);
+            Lbl_Total.TabIndex = 2;
+            // 
+            // Form_JogosFavoritos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1108, 559);
+            Controls.Add(Lbl_Total);
+            Controls.Add(Dgv_JogosFavoritos);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form_JogosFavoritos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Meus Jogos Favoritos";
+            ((System.ComponentModel.ISupportInitialize)Dgv_JogosFavoritos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)readJogosDtoBindingSource).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView Dgv_JogosFavoritos;
+        private BindingSource readJogosDtoBindingSource;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn descricaoDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn generoDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn enderecoImagemDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn anoLancamentoDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn plataformaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn notaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn ativoDataGridViewTextBoxColumn;
+        private Label Lbl_Total;
     }
 }
--- CarteiraDeJogosForms/Forms/Jogos/Form_JogosFavoritos.cs
+++ CarteiraDeJogosForms/Forms/Jogos/Form_JogosFavoritos.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using CarteiraDeJogos.Data.Dto.Jogos;
+using CarteiraDeJogosForms.Classes;
+using Newtonsoft.Json;
 
-namespace CarteiraDeJogosForms.Forms.Jogos
+namespace CarteiraDeJogosForms.Forms.Jogos;
+
+public partial class Form_JogosFavoritos : Form
 {
-    public partial class Form_JogosFavoritos : Form
+    private int usuarioId;
+    private string usuarioEmail;
+    private HttpClientBuilder _httpClientBuilder;
+    public Form_JogosFavoritos(HttpClientBuilder httpCliente, int usuarioId, string usuarioEmail)
     {
-        public Form_JogosFavoritos()
+        _httpClientBuilder = httpCliente;
+        this.usuarioId = usuarioId;
+        this.usuarioEmail = usuarioEmail;
+        PreencherListaDeJogosDoUsuario();
+        InitializeComponent();
+    }
+    private async void PreencherListaDeJogosDoUsuario()
+    {
+        HttpResponseMessage resposta = await _httpClientBuilder.GetJogosFavoritosDoUsuario(usuarioId);
+
+        List<int> listaDeJogos = JsonConvert.DeserializeObject<List<int>>(await resposta.Content.ReadAsStringAsync())!;
+        if (listaDeJogos.Count == 0)
         {
-            InitializeComponent();
+            Dgv_JogosFavoritos.DataSource = null;
+            Lbl_Total.Text = "Nenhum jogo marcado como favorito.";
+        }
+        else
+        {
+            List<ReadJogosDto> jogos = new List<ReadJogosDto>();
+            foreach (var item in listaDeJogos)
+            {
+                HttpResponseMessage respostaJogo = await _httpClientBuilder.GetJogo(item);
+                ReadJogosDto jogo = JsonConvert.DeserializeObject<ReadJogosDto>(await respostaJogo.Content.ReadAsStringAsync())!;
+                jogos.Add(jogo);
+            }
+            Dgv_JogosFavoritos.DataSource = jogos;
+            Lbl_Total.Text = $"{jogos.Count} jogos marcados como favorito.";
+            if (jogos.Count == 1) Lbl_Total.Text = $"{jogos.Count} jogos marcados como favorito.";
         }
     }
 }
