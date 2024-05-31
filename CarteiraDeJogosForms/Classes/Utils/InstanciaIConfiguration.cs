04496acd7744673339ed838f61ab04b5395c5166
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-05-31 20:28:09 -0300

    Refatoração do iconfiguration
    Refatoração do iconfiguration

---
+++ CarteiraDeJogosForms/Classes/Utils/InstanciaIConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CarteiraDeJogosForms.Classes.Utils;
+
+public static class InstanciaIConfiguration
+{
+    public static IConfiguration GetInstancia()
+    {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        return builder.Build();
+    }
+}
