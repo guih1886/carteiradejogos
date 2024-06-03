1465c9731cbe572fd2e5481f3ff491facca1984c
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-03 19:16:24 -0300

    Update Resources.Designer.cs

--- CarteiraDeJogosForms/Properties/Resources.Designer.cs
+++ CarteiraDeJogosForms/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace CarteiraDeJogosForms.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap buscar {
+            get {
+                object obj = ResourceManager.GetObject("buscar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cadastro {
             get {
                 object obj = ResourceManager.GetObject("cadastro", resourceCulture);
