0546b7b00a00fb9d60a2d5ba244eb9af2cd02658
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-03 19:46:46 -0300

    Create BuscarJogosDoUsuario.cs

---
+++ CarteiraDeJogosForms/Classes/Utils/BuscarJogosDoUsuario.cs
@@ -0,0 +1,21 @@
+﻿using CarteiraDeJogos.Data.Dto.Jogos;
+using Newtonsoft.Json;
+
+namespace CarteiraDeJogosForms.Classes.Utils;
+
+public static class BuscarJogosDoUsuario
+{
+    public static async Task<List<ReadJogosDto>> BuscarTodosJogosDoUsuario(HttpClientBuilder httpClient, int id)
+    {
+        HttpResponseMessage jogosResponse = await httpClient.GetJogosDoUsuario(id);
+        List<int> lista = JsonConvert.DeserializeObject<List<int>>(await jogosResponse.Content.ReadAsStringAsync())!;
+        List<ReadJogosDto> novaLista = new List<ReadJogosDto>();
+        foreach (int item in lista)
+        {
+            HttpResponseMessage resposta = await httpClient.GetJogo(item);
+            ReadJogosDto jogo = JsonConvert.DeserializeObject<ReadJogosDto>(await resposta.Content.ReadAsStringAsync())!;
+            novaLista.Add(jogo);
+        }
+        return novaLista;
+    }
+}
