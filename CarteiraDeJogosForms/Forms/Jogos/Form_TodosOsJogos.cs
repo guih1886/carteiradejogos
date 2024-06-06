9bf6891fb52a5131047f37f47828e49b85229335
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-06 12:56:25 -0300

    Refatoração do formulário listagem de jogos
    Refatoração do formulário listagem de jogos

--- CarteiraDeJogosForms/Forms/Jogos/Form_TodosOsJogos.cs
+++ CarteiraDeJogosForms/Forms/Jogos/Form_TodosOsJogos.cs
@@ -1,28 +1,24 @@
 ﻿using CarteiraDeJogos.Data.Dto.Jogos;
 using CarteiraDeJogosForms.Classes.Interfaces;
-using Newtonsoft.Json;
+using CarteiraDeJogosForms.Classes.Utils;
 
 namespace CarteiraDeJogosForms.Forms.Jogos
 {
     public partial class Form_TodosOsJogos : Form
     {
-        private int usuarioId;
-        private string usuarioEmail;
         private IHttpClient _httpClientBuilder;
-        public Form_TodosOsJogos(IHttpClient httpClient, int usuarioId, string usuarioEmail)
+        private int usuarioId;
+        public Form_TodosOsJogos(IHttpClient httpClient, int usuarioId)
         {
             _httpClientBuilder = httpClient;
             this.usuarioId = usuarioId;
-            this.usuarioEmail = usuarioEmail;
             InitializeComponent();
             PreencherListaDeJogosDoUsuario();
         }
 
         private async void PreencherListaDeJogosDoUsuario()
         {
-            HttpResponseMessage resposta = await _httpClientBuilder.GetRequisition($"/JogosDoUsuario/{usuarioId}/todosJogos");
-
-            List<int> listaDeJogos = JsonConvert.DeserializeObject<List<int>>(await resposta.Content.ReadAsStringAsync())!;
+            List<ReadJogosDto> listaDeJogos = await BuscarJogosDoUsuario.BuscarJogos($"/JogosDoUsuario/{usuarioId}/todosJogos", _httpClientBuilder);
             if (listaDeJogos.Count == 0)
             {
                 Dgv_Jogos.DataSource = null;
@@ -30,19 +26,8 @@ namespace CarteiraDeJogosForms.Forms.Jogos
             }
             else
             {
-                List<ReadJogosDto> jogos = new List<ReadJogosDto>();
-                foreach (var item in listaDeJogos)
-                {
-                    HttpResponseMessage respostaJogo = await _httpClientBuilder.GetRequisition($"/Jogos/{item}");
-                    if (respostaJogo.IsSuccessStatusCode)
-                    {
-                        ReadJogosDto jogo = JsonConvert.DeserializeObject<ReadJogosDto>(await respostaJogo.Content.ReadAsStringAsync())!;
-                        jogos.Add(jogo);
-                    }
-                }
-                Dgv_Jogos.DataSource = jogos.OrderBy(j => j.Id).ToList();
-                Lbl_Total.Text = $"{jogos.Count} jogos cadastrados.";
-                if (jogos.Count == 1) Lbl_Total.Text = $"{jogos.Count} jogo cadastrado.";
+                Dgv_Jogos.DataSource = listaDeJogos;
+                Lbl_Total.Text = $"{listaDeJogos.Count} jogo(s) cadastrado(s).";
             }
         }
     }
